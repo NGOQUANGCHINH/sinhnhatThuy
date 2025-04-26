91a088e540567e85239530eaa40a0d9a9b63f543
hoichammm <ngoquangchinh2005@gmail.com>
2025-04-27 01:33:54 +0700

    Update code: sửa chức năng XYZ

--- script.js
+++ script.js
@@ -8,4 +8,13 @@ window.addEventListener("DOMContentLoaded", () => {
     audio.play();
     startBtn.style.display = "none"; // Ẩn nút sau khi bấm
   });
+}); // script.js
+const startBtn = document.getElementById("start-btn");
+const overlay = document.getElementById("overlay");
+const bgMusic = document.getElementById("bgMusic");
+
+startBtn.addEventListener("click", () => {
+  overlay.style.display = "none"; // Ẩn lớp khóa
+  bgMusic.muted = false; // Bật tiếng
+  bgMusic.play(); // Phát nhạc luôn
 });
